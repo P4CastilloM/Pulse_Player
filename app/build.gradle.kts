30f3af5d9c43313880ce53387607c24a60bd0acf
P4CastilloM <119819369+P4CastilloM@users.noreply.github.com>
2025-07-09 22:37:39 -0400

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.media3.session)
     implementation(libs.androidx.lifecycle.process)
-    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
